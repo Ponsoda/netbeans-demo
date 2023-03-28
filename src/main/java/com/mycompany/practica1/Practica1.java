2d951ca7556b7858faa9f18db5815999716ccf28
PonsodaWegaw <ignacio.ponsoda@wegaw.com>
2023-03-28 10:21:44 +0200

    cambiando los nombres de las variables CalculoNotas y comprobandoNotas

---
+++ src/main/java/com/mycompany/practica1/Practica1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.practica1;
+import practica1.notas;
+
+/**
+ * Clase de la práctica1
+ */
+public class Practica1 {
+    /**
+     * Mostrando las notas por pantalla
+     * @param args Array de strings
+     */
+    public static void main(String[] args) {
+        notas nota = new notas();
+        nota.Mostrar();
+    }
+}
